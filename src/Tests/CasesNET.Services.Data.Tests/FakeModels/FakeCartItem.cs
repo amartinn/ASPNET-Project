a780cce6f11430c14867735ac90062efc269ee7d
=amartinn <martoy123691@gmail.com>
2020-12-16 02:00:46 +0200

    ignoring cached files'

--- src/Tests/CasesNET.Services.Data.Tests/FakeModels/FakeCartItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CasesNET.Services.Data.Tests
-{
-    using CasesNET.Data.Models;
-    using CasesNET.Services.Mapping;
-
-    public class FakeCartItem : IMapFrom<CartItem>
-    {
-    }
-}
